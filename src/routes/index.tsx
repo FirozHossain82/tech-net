3d14115ff549c20a717fc3b2383dbb1554d5df84
FirozHossain82 <firozhossain295@gmail.com>
2023-08-16 19:22:27 +0600

    redux toolkit install and setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../page/home";
 import App from "../App";
+import Login from "../page/Login";
+import NotFound from "../page/NotFound";
 
 const router =createBrowserRouter ( [
     {
         path:'/',
         element:<App/>
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    },
+    {
+      path:'*',
+      element:<NotFound/>   
     }
 ]);
 
