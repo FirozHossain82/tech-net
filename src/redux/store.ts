10fe2833baba019beb68c4eba152a6040f5b3e2c
FirozHossain82 <firozhossain295@gmail.com>
2023-08-19 12:26:47 +0600

    middleware setup practice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,17 @@
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+import logger from "./middlewares/logger";
+
+
+
 const store = configureStore({
-    reducer:{
-        counter : counterReducer,
-    },
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
